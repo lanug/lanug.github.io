30c3df49645cfabcef3574a0ca98750e1ac1078c
X Zhang <zhangxd6@hotmail.com>
2017-03-31 22:44:30 -0500

    add map and banner update other pages with notification

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(function() {
-
     $(window).trigger('resize');
+
 });
 
 $(window).resize(function() {
     $("section").height($(window).height() - 100);
+    $(".slide").height($(window).height() - 100);
+    $('#usa').width($('body').width() - 30)
 })
 
 setInterval(function() {
     var winWidth = $(window).width();
     if (winWidth < 1000) {
         $("section").height('auto')
+        $(".slide").height('auto')
     }
 }, 250);
