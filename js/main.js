754d6bb57f7979fb71a38a21d7172ffc73205605
X Zhang <zhangxd6@hotmail.com>
2017-06-30 09:12:33 -0500

    update the date

--- js/main.js
+++ js/main.js
@@ -3,16 +3,21 @@ $(function() {
 
 });
 
-$(window).resize(function() {
-    $("section").height($(window).height() - 100);
-    $(".slide").height($(window).height() - 100);
-    $('#usa').width($('body').width() - 30)
+$(window).resize(function () {
+    var winWidth = $(window).width();
+    if (winWidth > 1000) {
+        $("section").height($(window).height() - 100);
+        $(".slide").height($(window).height() - 100);
+        $('#usa').width($('body').width() - 30)
+    }
 })
 
 setInterval(function() {
     var winWidth = $(window).width();
-    if (winWidth < 1000 && $("section").width()!==winWidth) {
-        $("section").height('auto')
-        $(".slide").height('auto')
+    if (winWidth < 1000 && $("section").width() !== winWidth) {
+        $("section").attr('style', '');
+        $(".slide").attr('style', '');
+        // $("section").height('auto')
+        // $(".slide").height('auto')
     }
 }, 250);
